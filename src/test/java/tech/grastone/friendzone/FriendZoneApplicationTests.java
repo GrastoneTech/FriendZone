618d30130655a4f9cd528cf3c740ab1b2137ad93
AshishGaikwad <ashishgaikwad6268@gmail.com>
2021-05-11 19:20:48 +0530

    Initial Project startup

---
+++ src/test/java/tech/grastone/friendzone/FriendZoneApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.grastone.friendzone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendZoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
